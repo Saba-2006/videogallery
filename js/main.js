b5d5f39c223853d4fe0fd58c13b84bf2b2e35aaf
Saba-2006 <130082949+Saba-2006@users.noreply.github.com>
2023-09-19 22:59:14 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,4 +6,20 @@ menuToggle.addEventListener("click", () => {
   showcase.classList.toggle("active");
 });
 
+/* show more */
 
+var video = document.querySelectorAll(".video");
+var btn = document.querySelector(".btn");
+var currentvideo = 9;
+btn.addEventListener("click", function () {
+  for (var i = currentvideo; i < currentvideo + 3; i++) {
+    if (video[i]) {
+      video[i].style.display = "block";
+    }
+  }
+
+  currentvideo += 3;
+  if (currentvideo >= video.length) {
+    event.target.style.display = "none";
+  }
+});
